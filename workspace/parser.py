d8cf154a8fb794bc142fa9eaf34f6d4cf69f4443
Guangting Yu <yugtmath@umich.edu>
2018-06-12 23:45:01 +0800

    passage parsing complete, ready to do paragraph and sentence

--- workspace/parser.py
+++ workspace/parser.py
@@ -9,23 +9,34 @@ class writing(object):
         self.outpath = outpath
         self.lines = file.readlines()
         file.close()
+        for i in range(len(self.lines)):
+            self.lines[i] = self.lines[i].rstrip()
+            self.lines[i] = self.lines[i].replace('\x0c','')
+            self.lines[i] = self.lines[i].replace('\n','')
+        self.__issue()
 
-    def issue(self):
+
+    def __issue(self):
         temp = []
         for line in self.lines:
             if line[0:5] == "Issue":
                 self.empty_line = False
                 temp.append(line)
-            elif line[0] == '\n' or line == '':
+            elif len(line)==0:
                 if ~self.empty_line:
                     self.empty_line = True
                     self.passages.append(temp)
                     temp = []
             else:
                 temp.append(line)
-        self.passages = filter(lambda x: len(x)>0, self.passages)
-        return self.passages
+        self.passages = [p for p in self.passages if len(p)>0]
+        return
+
+    def issueN(self, n):
+        issue = self.passages[n]
+        return issue
+
 
 if __name__ == "__main__":
     w = writing("./workspace/issue.txt","")
-    print(w.issue())
+    print(w.issueN(0))
